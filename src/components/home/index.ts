818d81b8a31208d63dc4f2ccb36656981ca28c31
branewaveai <157820981+branewaveai@users.noreply.github.com>
2024-03-03 18:56:49 +0530

    Add files via upload

---
+++ src/components/home/index.ts
@@ -0,0 +1,7 @@
+export { default as HomeHero } from './hero'
+export { default as HomePopularCourse } from './popular-courses'
+export { default as HomeFeature } from './feature'
+export { default as HomeTestimonial } from './testimonial'
+export { default as HomeOurMentors } from './mentors'
+export {default as HomeTreatment} from './treatment'
+export { default as HomeNewsLetter } from './newsletter'
---
+++ src/interfaces/mentor.ts
@@ -0,0 +1,11 @@
+import type { User } from './user'
+
+export interface Mentor extends Omit<User, 'professional'> {
+  description?: string
+  location?: string
+  category: string
+  company?: {
+    name: string
+    logo: string
+  }
+}
